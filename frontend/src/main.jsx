d062815a06d08e3379afbae441dc5863572483b9
codead777 <arghadey2006@gmail.com>
2025-08-12 16:16:33 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,3 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(<App />)
 <main>
   <div className="text-center mt-10 text-gray-700">App loaded successfully!</div>
 </main>
